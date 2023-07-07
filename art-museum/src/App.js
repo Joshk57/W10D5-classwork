096c22619f03f65efdaae579dce9f78c5ceed708
Michelle <rphmichelle@gmail.com>
2023-07-07 15:24:16 -0400

    starting art museum project

---
+++ art-museum/src/App.js
@@ -0,0 +1,13 @@
+import harvardArt from "./data/harvardArt";
+import GalleryNavigation from "./components/GalleryNavigation/GalleryNavigation";
+
+function App() {
+  return (
+    <>
+      <h1>Hello from App</h1>
+      <GalleryNavigation />
+    </>
+  );
+}
+
+export default App;
---
+++ art-museum/src/components/GalleryNavigation/GalleryNavigation.js
@@ -0,0 +1,8 @@
+import harvardArt from "../../data/harvardArt";
+
+function GalleryNavigation(props) {
+    const galleries = Object.values(harvardArt);
+    console.log(galleries)
+}
+
+export default GalleryNavigation;
