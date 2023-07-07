9a999a83dfddfd8a705ebad020675e2d83563d46
Michelle <rphmichelle@gmail.com>
2023-07-07 16:01:31 -0400

    working on gallery navigation

--- art-museum/src/components/GalleryNavigation/GalleryNavigation.js
+++ art-museum/src/components/GalleryNavigation/GalleryNavigation.js
@@ -1,8 +1,18 @@
+import { NavLink } from "react-router-dom/cjs/react-router-dom.min";
 import harvardArt from "../../data/harvardArt";
 
 function GalleryNavigation(props) {
-    const galleries = Object.values(harvardArt);
-    console.log(galleries)
+    const galleries = Object.values(harvardArt.records);
+    const gallerylist =
+        galleries.map(gallery => {
+            return <NavLink to={`/galleries/${gallery.id}`}>{gallery.gallerynumber}</NavLink>
+        })
+    return (
+        <nav>
+            {gallerylist}
+        </nav>
+    )
+
 }
 
 export default GalleryNavigation;
